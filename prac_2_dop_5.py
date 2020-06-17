c94d4cd8c785d81fa81cfd869fd0ac218612f922
NPavelka <66479468+NPavelka@users.noreply.github.com>
2020-06-17 10:55:11 +0200

    Практикум №2, Дополнительное задание №5

---
+++ prac_2_dop_5.py
@@ -0,0 +1,21 @@
+print('----------------------------Dop_Zadanie_5-----------------------------')
+print('Вывод простых множителей числа')
+
+n = int(input('Введите число: ' ))
+
+while n > 1:
+    d = 2
+    stop = 0
+    
+    while stop != 1:
+        if n % d == 0:
+            n = n // d
+            print(d, end = ' ')
+            stop = 1
+            break
+        
+        else:
+            d = d + 1
+            
+    if stop == 1:
+        continue            
