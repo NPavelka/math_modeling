3b26d464da4bf5e96c09831d310606329eb0a6c2
NPavelka <66479468+NPavelka@users.noreply.github.com>
2020-07-03 14:19:22 +0200

    Константы

---
+++ astro_constans.py
@@ -0,0 +1,26 @@
+
+earth_mass = 5.94*10**24            #кг
+
+earth_radius = 6378140              #м
+
+earth_year = 365.24219              #дни
+
+earth_sid_period = 86164            #с
+
+gravity_constans = 6.67*10**(-11)   #м**3/(кг * с**2)
+
+speed_light = 2.998*10**8           #м/с
+
+plank_constans = 6.626*10**(-34)    #кг*м**2/с
+
+stefan_constant = 5.67*10**(-8)     #кг/(с**3 * К**4)
+
+astronomical_unit = 1.496*10**16    #м
+
+sun_mass = 1.989*10**30             #кг
+
+sun_radius = 697*10**6              #м
+
+moon_mass = 7.348*10**22            #кг
+
+moon_radius = 1.738*10**6           #м
